7446bebf5617f43bfcd00084cba2d3006dad1bc1
HDzungx <hdzungx@gmail.com>
2024-02-08 21:11:51 +0700

    miuicamera: add device features, some prebuilt vendor libs and update libs to 14.0.9 CN
    Signed-off-by: HDzungx <hdzungx@gmail.com>

--- Android.mk
+++ Android.mk
@@ -10,10 +10,10 @@ CAMERA_LIBRARIES := libcamera_algoup_jni.xiaomi.so libcamera_mianode_jni.xiaomi.
 
 CAMERA_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/MiuiCamera/lib/arm64/,$(notdir $(CAMERA_LIBRARIES)))
 $(CAMERA_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-        @echo "MiuiCamera lib link: $@"
-        @mkdir -p $(dir $@)
-        @rm -rf $@
-        $(hide) ln -sf /system/lib64/$(notdir $@) $@
+	@echo "MiuiCamera lib link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /system/lib64/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(CAMERA_SYMLINKS)
 
